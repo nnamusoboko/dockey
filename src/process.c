6d442f64c504fa46dbed5c7b68981ff00f6847c9
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-03-21 02:49:47 +0300

    feat: fork process[parent, child]

--- src/process.c
+++ src/process.c
@@ -1,9 +1,29 @@
 #include "common.h"
 #include "process.h"
+#include "error.h"
+#include <sys/types.h>
 
 pid_t spwan_container(struct container_config *config) {
-    (void)config;
-    return -1;
+    pid_t pid;
+    
+    pid = fork();
+    if (pid < 0) {
+        pdie("fork");
+    }
+
+    if (pid == 0) {
+        /* Child process*/
+        execvp(config->argv[0], config->argv);
+        
+        /* 
+         * This reached once execvp() fails
+         * On success, execvp() never returns
+         *
+         */
+        pdie("execvp"); 
+    }
+    /* Parent processs gets child PID */
+    return pid;
 }
 
 int child_entry(void *arg) {
