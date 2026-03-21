fc4b1ccac2cec27970d12d0d4d56d37bab7550da
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-03-21 18:44:50 +0300

    feat: set UTS, PID, mount namespaces, allocate stack space

--- src/process.c
+++ src/process.c
@@ -4,25 +4,36 @@
 #include <sys/types.h>
 
 pid_t spawn_container(struct container_config *config) {
+    int flags;
+    void *stack;
+    void *stack_top;
     pid_t pid;
-    
-    pid = fork();
-    if (pid < 0) {
-        pdie("fork");
+    struct child_context *ctx;
+
+    flags = SIGCHILD;
+
+    if (config->use_pid_ns) {
+        flags |= CLONE_NEWPID;
     }
 
-    if (pid == 0) {
-        /* Child process*/
-        execvp(config->argv[0], config->argv);
-        
-        /* 
-         * This reached once execvp() fails
-         * On success, execvp() never returns
-         *
-         */
-        pdie("execvp"); 
+    if (config->use_uts_ns) {
+        flags |= CLONE_NEWUTS;
     }
-    /* Parent processs gets child PID */
+
+    if (config->use_mount_ns) {
+        flags |= CLONE_NEWNS;
+    }
+
+    stack = xmalloc(STACK_SIZE);
+    stack_top = (char *)stack + STACK_SIZE;
+
+    ctx = xmalloc(sizeof(*ctx));
+    ctx->config = config;
+
+    pid = clone(child_entry, stack_top, flags, ctx);
+    if (pid < 0) {
+        pdie("clone");
+    }
     return pid;
 }
 
