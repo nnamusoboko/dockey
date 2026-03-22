5a5bbfbcc8b2d5c5bb4a7ef8d8ede27e8732e8d4
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-03-22 14:58:13 +0300

    feat: change root and move to it

--- src/fs.c
+++ src/fs.c
@@ -1,7 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "fs.h"
 
+static void pdie(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
 int setup_rootfs(const struct container_config *config) {
-    (void)config;
+    if (chroot(config->rootfs) < 0) {
+        pdie("chroot");
+    }
+
+    if (chdir("/") < 0) {
+        pdie("chdir");
+    }
+
     return 0;
 }
 
