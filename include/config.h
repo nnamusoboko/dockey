5acc30f400a0c15fa1e07027186fd9c91afafc06
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-03-28 01:23:57 +0300

    feat: tell runtime to use user namespace and mappings

--- include/config.h
+++ include/config.h
@@ -2,6 +2,7 @@
 #define CONFIG_H
 
 #include <stdbool.h>
+#include <sys/types.h>
 
 struct container_config {
     char *hostname;
@@ -11,7 +12,11 @@ struct container_config {
 
     bool use_pid_ns;
     bool use_uts_ns;
-    bool use_mount_ns;    
+    bool use_mount_ns;
+    bool use_user_ns;
+
+    uid_t host_uid;
+    gid_t host_gid;
 };
 
 #endif
